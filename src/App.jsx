8af22ef4a12cf312aaf574c97bb4923170d7bdf5
Gabe Eaton <eatong13@gmail.com>
2024-07-25 20:34:07 -0500

    add cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,146 @@ import axios from "axios";
 import "./App.css";
 import { api_key } from "./api";
 
-
 function App() {
   const [search, setSearch] = useState("");
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [infoError, setInfoError] = useState("");
+  const [forecast, setForecast] = useState("");
 
-    const submitForm = async (e) => {
-      e.preventDefault();
-      try {
-        const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${api_key}`;
-        const response = await axios.get(API_URL);
-          setData(response.data);
-      } catch (err) {
-        console.error(err.message);
-      }
+  const submitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${api_key}`;
+      const response = await axios.get(API_URL);
+      setData(response.data);
+
+      const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${search}&appid=${api_key}`;
+      const forecastResponse = await axios.get(forecastURL);
+      setForecast(forecastResponse.data);
+     
+      console.log(forecastResponse.data);
+      console.log(response.data);
+    } catch (err) {
+      console.error(err.message);
+      setData(null);
+      setError("City Not Found");
+      setInfoError("");
     }
-
-    useEffect(() => {
-      if(data) {
-        setData(data);
-      }
-    }, [data]);
+  };
+  const convertTemp = (kelvin) => {
+    return (((kelvin - 273.15) * 9) / 5 + 32).toFixed(0);
+  };
   return (
     <>
       <div className="flex-container">
         <div className="flex-item">
-          <form onSubmit= {submitForm}>
+          <form onSubmit={submitForm}>
             <input
               className="search"
               type="search"
               onChange={(e) => setSearch(e.target.value)}
             />
-            <button className="search-btn" type="submit">Search</button>
+            <button className="search-btn" type="submit">
+              Search
+            </button>
           </form>
-          <h1>{data.main.hints}</h1>
+          <div className="temp-container">
+            <h1 className="main-temp">
+              {data ? <span>{convertTemp(data.main.temp)}°</span> : error}
+            </h1>
+          </div>
+          <div className="info-container">
+            <div className="info">
+              <div className="info-item">
+                <h6 className="temp-text">Temperature</h6>
+                <h5 className="temp-text">
+                  H:{" "}
+                  {data ? (
+                    <span>{convertTemp(data.main.temp_max)}° </span>
+                  ) : (
+                    infoError
+                  )}
+                </h5>
+                <h5 className="temp-text">
+                  L:{" "}
+                  {data ? (
+                    <span>{convertTemp(data.main.temp_min)}° </span>
+                  ) : (
+                    infoError
+                  )}
+                </h5>
+                <h5 className="temp-text">
+                  Feels Like:{" "}
+                  {data ? (
+                    <span>{convertTemp(data.main.feels_like)}°</span>
+                  ) : (
+                    infoError
+                  )}
+                </h5>
+              </div>
+              <div className="info-item">
+                <h6 className="temp-text">Humidity</h6>
+                <h5 className="temp-text">
+                  {" "}
+                  {data ? <span>{data.main.humidity}%</span> : infoError}
+                </h5>
+              </div>
+              <div className="info-item">
+                {" "}
+                <h6 className="temp-text">Weather</h6>
+                <h5 className="temp-text">
+                  {data ? (
+                    <span>
+                      {data.weather[0].main}: {data.weather[0].description}
+                    </span>
+                  ) : (
+                    infoError
+                  )}
+                </h5>
+              </div>
+              <div className="info-item">
+                <h6 className="temp-text">Wind</h6>
+                <h5 className="temp-text">
+                  Speed:{" "}
+                  {data ? (
+                    <span>{(data.wind.speed * 2.23694).toFixed(1)} mph</span>
+                  ) : (
+                    infoError
+                  )}
+                </h5>
+                <h5 className="temp-text">
+                  Degrees: {data ? <span>{data.wind.deg}°</span> : infoError}
+                </h5>
+                <h5 className="temp-text">
+                  Gust:{" "}
+                  {data ? (
+                    <span>{(data.wind.gust * 2.23694).toFixed(1)}mph</span>
+                  ) : (
+                    infoError
+                  )}
+                </h5>
+              </div>
+            </div>
+            <div className="week">
+              <div className = "three-hr">
+                 <h3>3 Hourly Forecast</h3>
+                 <div className = "three-hr-container">
+                  
+                 </div>
+              </div>
+              <div className = "daily">
+                  <h3>Daily Forecast</h3>
+                  <div className = "daily-container">
+                    <div className = "day">Mon</div>
+                    <div className = "day">hi</div>
+                    <div className = "day">hi</div>
+                    <div className = "day">hi</div>
+                    <div className = "day">hi</div>
+                  </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
