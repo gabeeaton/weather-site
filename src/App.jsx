e6ac84777bf0731cc39dde948e9ef419636ed343
Gabe Eaton <eatong13@gmail.com>
2024-08-04 18:18:58 -0500

    added city dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import "./App.css";
 import { api_key } from "./api";
 
 function App() {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState(""); //city
+  const [state, setState] = useState("");
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [infoError, setInfoError] = useState("");
@@ -38,9 +39,68 @@ function App() {
       <div className="flex-container">
         <div className="flex-item">
           <form onSubmit={submitForm}>
+            <select
+              className="state-drop"
+              onChange={(e) => setState(e.target.value)}
+              placeholder="Select a state"
+            >
+              <option value={null}>Select a state...</option>
+              <option value="AL">Alabama</option>
+              <option value="AK">Alaska</option>
+              <option value="AZ">Arizona</option>
+              <option value="AR">Arkansas</option>
+              <option value="CA">California</option>
+              <option value="CO">Colorado</option>
+              <option value="CT">Connecticut</option>
+              <option value="DE">Delaware</option>
+              <option value="DC">District Of Columbia</option>
+              <option value="FL">Florida</option>
+              <option value="GA">Georgia</option>
+              <option value="HI">Hawaii</option>
+              <option value="ID">Idaho</option>
+              <option value="IL">Illinois</option>
+              <option value="IN">Indiana</option>
+              <option value="IA">Iowa</option>
+              <option value="KS">Kansas</option>
+              <option value="KY">Kentucky</option>
+              <option value="LA">Louisiana</option>
+              <option value="ME">Maine</option>
+              <option value="MD">Maryland</option>
+              <option value="MA">Massachusetts</option>
+              <option value="MI">Michigan</option>
+              <option value="MN">Minnesota</option>
+              <option value="MS">Mississippi</option>
+              <option value="MO">Missouri</option>
+              <option value="MT">Montana</option>
+              <option value="NE">Nebraska</option>
+              <option value="NV">Nevada</option>
+              <option value="NH">New Hampshire</option>
+              <option value="NJ">New Jersey</option>
+              <option value="NM">New Mexico</option>
+              <option value="NY">New York</option>
+              <option value="NC">North Carolina</option>
+              <option value="ND">North Dakota</option>
+              <option value="OH">Ohio</option>
+              <option value="OK">Oklahoma</option>
+              <option value="OR">Oregon</option>
+              <option value="PA">Pennsylvania</option>
+              <option value="RI">Rhode Island</option>
+              <option value="SC">South Carolina</option>
+              <option value="SD">South Dakota</option>
+              <option value="TN">Tennessee</option>
+              <option value="TX">Texas</option>
+              <option value="UT">Utah</option>
+              <option value="VT">Vermont</option>
+              <option value="VA">Virginia</option>
+              <option value="WA">Washington</option>
+              <option value="WV">West Virginia</option>
+              <option value="WI">Wisconsin</option>
+              <option value="WY">Wyoming</option>
+            </select>
             <input
               className="search"
               type="search"
+              placeholder="Enter a city..."
               onChange={(e) => setSearch(e.target.value)}
             />
             <button className="search-btn" type="submit">
@@ -117,7 +177,7 @@ function App() {
                 <h5 className="temp-text">
                   Gust:{" "}
                   {data ? (
-                    <span>{(data.wind.gust * 2.23694).toFixed(1)}mph</span>
+                    <span>{(data.wind.gust * 2.23694).toFixed(1)} mph</span>
                   ) : (
                     infoError
                   )}
