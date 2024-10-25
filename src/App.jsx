65d479c2c3d08921e1c2cb7631ac347819855e5e
Gabe Eaton <eatong13@gmail.com>
2024-10-25 09:56:55 -0400

    added backgrounds

--- src/App.jsx
+++ src/App.jsx
@@ -183,8 +183,8 @@ function App() {
                     <p>{day}</p>
                   </div>
                   <div className="temp"> {/*renders the icon matching the description*/}
-                    {forecast.list[index * 8].weather[0].description === 
-                    "clear sky" ? (
+                    {forecast.list[index * 8].weather[0].description ===
+                      "clear sky" ? (
                       <>
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
@@ -198,8 +198,8 @@ function App() {
                         </svg>
                       </>
                     ) : forecast.list[
-                        index * 8
-                      ].weather[0].description.includes("clouds") ? (
+                      index * 8
+                    ].weather[0].description.includes("clouds") ? (
                       <>
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
@@ -213,8 +213,8 @@ function App() {
                         </svg>
                       </>
                     ) : forecast.list[
-                        index * 8
-                      ].weather[0].description.includes("rain") ? (
+                      index * 8
+                    ].weather[0].description.includes("rain") ? (
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="27"
@@ -226,8 +226,8 @@ function App() {
                         <path d="M4.158 12.025a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m3 0a.5.5 0 0 1 .316.633l-1 3a.5.5 0 0 1-.948-.316l1-3a.5.5 0 0 1 .632-.317m3 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m3 0a.5.5 0 0 1 .316.633l-1 3a.5.5 0 1 1-.948-.316l1-3a.5.5 0 0 1 .632-.317m.247-6.998a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 11H13a3 3 0 0 0 .405-5.973M8.5 2a4 4 0 0 1 3.976 3.555.5.5 0 0 0 .5.445H13a2 2 0 0 1 0 4H3.5a2.5 2.5 0 1 1 .605-4.926.5.5 0 0 0 .596-.329A4 4 0 0 1 8.5 2" />
                       </svg>
                     ) : forecast.list[
-                        index * 8
-                      ].weather[0].description.includes("thunderstorm") ? (
+                      index * 8
+                    ].weather[0].description.includes("thunderstorm") ? (
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="27"
@@ -239,8 +239,8 @@ function App() {
                         <path d="M13.405 4.027a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 10H13a3 3 0 0 0 .405-5.973M8.5 1a4 4 0 0 1 3.976 3.555.5.5 0 0 0 .5.445H13a2 2 0 0 1 0 4H3.5a2.5 2.5 0 1 1 .605-4.926.5.5 0 0 0 .596-.329A4 4 0 0 1 8.5 1M7.053 11.276A.5.5 0 0 1 7.5 11h1a.5.5 0 0 1 .474.658l-.28.842H9.5a.5.5 0 0 1 .39.812l-2 2.5a.5.5 0 0 1-.875-.433L7.36 14H6.5a.5.5 0 0 1-.447-.724z" />
                       </svg>
                     ) : forecast.list[
-                        index * 8
-                      ].weather[0].description.includes("snow") ? (
+                      index * 8
+                    ].weather[0].description.includes("snow") ? (
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="27"
@@ -372,22 +372,23 @@ function App() {
             </div>
           </div>
         </div>
-        
+
         <div>
-      {videoSrc && (
-        <video key={videoKey} autoPlay muted loop className="background-video">{/*displays video*/}
-          <source src={videoSrc} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      )}
-    </div>
+          {videoSrc && (
+            <video key={videoKey} autoPlay muted loop className="background-video">{/*displays video*/}
+              <source src={videoSrc} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          )}
+        </div>
       </div>
     </div>
   );
 
 }
 
-export const lineGraphData = (forecastResponse) => {{/*passes line graph data*/}
+export const lineGraphData = (forecastResponse) => {
+  {/*passes line graph data*/ }
   if (forecastResponse) {
     return {
       labels: forecastResponse.list.slice(0, 13).map((entry) =>
@@ -408,7 +409,8 @@ export const lineGraphData = (forecastResponse) => {{/*passes line graph data*/}
   }
 };
 
-export const DoughnutData = (humidity) => {{/*passes donut data*/}
+export const DoughnutData = (humidity) => {
+  {/*passes donut data*/ }
   if (humidity) {
     return {
       labels: ["Humidity"],
@@ -426,7 +428,8 @@ export const DoughnutData = (humidity) => {{/*passes donut data*/}
   }
 };
 
-export const BarData = (temp) => {{/*passes bar graph data*/}
+export const BarData = (temp) => {
+  {/*passes bar graph data*/ }
   if (temp) {
     return {
       labels: ["High", "Low", "Feels Like"],
